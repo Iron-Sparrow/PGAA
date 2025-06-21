02db332c3061e625f5a4224462265511b624e485
Adam Kudelas <adamkokudelas@gmail.com>
2025-06-22 01:03:42 +0200

    Add new SSAA functions and performance assertions for Pygame compatibility

--- src/PGAA/SuperSampling.py
+++ src/PGAA/SuperSampling.py
@@ -42,6 +42,13 @@ def aa05_hq(surf: pg.Surface) -> pg.Surface:
     )
     return pg.transform.scale2x(pg.transform.smoothscale_by(surf, 0.5))
 
+def aa05_lq(surf: pg.Surface) -> pg.Surface:
+    assert isinstance(surf, pg.Surface)
+    assert surf.get_width() % 2 == 0 and surf.get_height() % 2 == 0, (
+        "Surface dimensions must be even for SSAA."
+    )
+    return pg.transform.scale_by(pg.transform.scale_by(surf, 0.5), 2)
+
 
 def aa2(surf: pg.Surface) -> pg.Surface:
     """
@@ -64,6 +71,9 @@ def aa2_hq(surf: pg.Surface) -> pg.Surface:
     assert isinstance(surf, pg.Surface)
     return pg.transform.smoothscale_by(pg.transform.scale2x(surf), 0.5)
 
+def aa2_lq(surf: pg.Surface) -> pg.Surface:
+    assert isinstance(surf, pg.Surface)
+    return pg.transform.scale_by(pg.transform.scale_by(surf, 2), 0.5)
 
 def aa4(surf: pg.Surface) -> pg.Surface:
     """
@@ -97,6 +107,9 @@ def aa8(surf: pg.Surface) -> pg.Surface:
     :return: pygame.Surface: A new surface with SSAA applied.
     """
     assert isinstance(surf, pg.Surface)
+    assert pg.version.vernum.major >= 2 and pg.version.vernum.minor >= 4, (
+        "SSAA8x requires Pygame-CE 2.4.0 or later. The hardware acceleration is required."
+    )
     return pg.transform.scale_by(pg.transform.scale_by(surf, 8), 0.125)
 
 def aa8_hq(surf: pg.Surface) -> pg.Surface:
@@ -110,4 +123,13 @@ def aa8_hq(surf: pg.Surface) -> pg.Surface:
     :return: pygame.Surface: A new surface with SSAA applied.
     """
     assert isinstance(surf, pg.Surface)
+    assert pg.version.vernum.major >= 2 and pg.version.vernum.minor >= 4, (
+        "SSAA8x requires Pygame-CE 2.4.0 or later. The hardware acceleration is required."
+    )
     return pg.transform.smoothscale_by(pg.transform.smoothscale_by(surf, 8), 0.125)
+
+def aa32(surf: pg.Surface) -> pg.Surface:
+    assert isinstance(surf, pg.Surface)
+    if __debug__:
+        print("SSAA32x is for benchmarking purposes only, it is not to be used in any code not involving benchmarking.")
+    return pg.transform.smoothscale_by(pg.transform.smoothscale_by(surf, 32), (1 / 32))
